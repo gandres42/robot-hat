7c4854bcfcdeb0e3775e5b43da48cceb4b24037b
gandres42 <gandres2416@gmail.com>
2025-01-21 13:26:36 -0800

    filedb creates files if they don't exist already

--- robot_hat/filedb.py
+++ robot_hat/filedb.py
@@ -1,15 +1,21 @@
 import json
+import os
 
 class fileDB(object):
 	"""
 	A file based database, read and write arguements in the specific file.
 	"""
-	def __init__(self, db_file:str):
+	def __init__(self, db_path:str):
 		"""
 		:param db_file: the file to save the data.
 		:type db_file: str
 		"""
-		self._db_file_path = db_file
+  
+		if not os.path.exists(db_path):
+			os.makedirs(os.path.dirname(db_path), exist_ok=True)
+			with open(db_path, "w") as file:
+				file.write("{}")
+		self._db_file_path = db_path
 
 	def get(self, key, default_value = None):
 		with open(self._db_file_path, "r") as db_file:
@@ -17,7 +23,10 @@ class fileDB(object):
 			if key in db:
 				return db[key]
 			else:
-				return default_value
+				if default_value is not None:
+					return default_value
+				else:
+					raise FileNotFoundError("key does not exist")
 
 	def set(self, key, value):
 		with open(self._db_file_path, "rw") as db_file:
